c42f1900a34bd00c72d154831dd14c830c347e53
miraz-mondal <mirazmondal.official@gmail.com>
2023-06-12 01:50:49 +0530

    changed the h1 color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 function dare() {
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <h1 className="text-4xl mb-7 font-semibold">NSFW DARE 😛</h1>
+      <h1 className="text-4xl mb-7 font-semibold text-white">NSFW DARE 😛</h1>
       <div className="border-2 md:h-3/6 md:w-3/6 h-3/6 w-3/4 mb-5 flex items-center justify-center bg-pink-300 text-white text-xl rounded">
         output
       </div>
